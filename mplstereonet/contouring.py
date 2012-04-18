482029d6cf9056b639d108d5fb272904ce050a0e
Joe Kington <joferkington@gmail.com>
2012-04-18 15:21:45 -0500

    Basic contouring support

--- mplstereonet/contouring.py
+++ mplstereonet/contouring.py
@@ -1,7 +1,5 @@
 import numpy as np
 import stereonet_math
-import matplotlib.mlab as mlab
-import scipy.signal
 import matplotlib.delaunay as delaunay
 
 
@@ -67,14 +65,10 @@ def contour_grid(*args, **kwargs):
             'schmidt':schmidt_count,
             'kamb':kamb_count,
             'exponential_kamb':exponential_kamb,
-            'fast_kde':gaussian_kde,
             }[method]
-    if method != 'fast_kde':
-        counter_lon, counter_lat, totals = count_points(lon, lat, func, sigma,
-                                                        num_counters)
-        xi, yi, zi = grid_data(counter_lon, counter_lat, totals)
-    else:
-        xi, yi, zi = gaussian_kde(lon, lat)
+    counter_lon, counter_lat, totals = count_points(lon, lat, func, sigma,
+                                                    num_counters)
+    xi, yi, zi = grid_data(counter_lon, counter_lat, totals)
     return xi, yi, zi
 
 def exponential_kamb(cos_dist, sigma=3):
@@ -115,61 +109,4 @@ def schmidt_count(cos_dist, sigma=None):
     radius = 0.01
     count = ((1 - cos_dist) <= radius).sum()
     return (count - 0.5) / (cos_dist.size * radius)
-
-def gaussian_kde(lon, lat, gridsize=(200,200)):
-    nrows, ncols = gridsize
-    bound = np.pi / 2.0
-    n = lon.size
-    xmin, xmax, ymin, ymax = -bound, bound, -bound, bound
-    dx = (xmax - xmin) / (ncols - 1)
-    dy = (ymax - ymin) / (nrows - 1)
-
-    # Next, make a 2D histogram of x & y
-    # Avoiding np.histogram2d due to excessive memory usage with many points
-    grid,xbins,ybins = np.histogram2d(lon, lat, bins=(ncols, nrows), 
-                            range=np.array([[xmin, xmax+dx],[ymin, ymax+dy]]))
-
-    # Calculate the covariance matrix (in pixel coords)
-    xyi = np.vstack((lon, lat)).T
-    xyi -= [xmin, ymin]
-    xyi /= [dx, dy]
-    cov = np.cov(xyi.T)
-
-    # Scaling factor for bandwidth
-    scotts_factor = np.power(n, -1.0 / 6) # For 2D
-
-    # First, determine how big the kernel needs to be
-    std_devs = np.diag(np.sqrt(cov))
-    kern_nx, kern_ny = np.round(scotts_factor * 2 * np.pi * std_devs)
-
-    # Determine the bandwidth to use for the gaussian kernel
-    inv_cov = np.linalg.inv(cov * scotts_factor**2) 
-
-    # x & y (pixel) coords of the kernel grid, with <x,y> = <0,0> in center
-    xx = np.arange(kern_nx, dtype=np.float) - kern_nx / 2.0
-    yy = np.arange(kern_ny, dtype=np.float) - kern_ny / 2.0
-    xx, yy = np.meshgrid(xx, yy)
-
-    # Then evaluate the gaussian function on the kernel grid
-    kernel = np.vstack((xx.flatten(), yy.flatten()))
-    kernel = np.dot(inv_cov, kernel) * kernel 
-    kernel = np.sum(kernel, axis=0) / 2.0 
-    kernel = np.exp(-kernel) 
-    kernel = kernel.reshape((kern_ny, kern_nx))
-
-    # Convolve the gaussian kernel with the 2D histogram, producing a gaussian
-    # kernel density estimate on a regular grid
-    grid = scipy.signal.fftconvolve(grid, kernel, mode='same')
-    grid[grid < 1e-8] = 0
-
-    # Normalization factor to divide result by so that units are in the same
-    # units as scipy.stats.kde.gaussian_kde's output.  
-    norm_factor = np.pi * cov * scotts_factor**2
-    norm_factor = np.linalg.det(norm_factor)
-    norm_factor = n * dx * dy * np.sqrt(norm_factor)
-
-    # Normalize the result
-    grid /= norm_factor 
-
-    return xbins[:-1], ybins[:-1], grid.T
 
