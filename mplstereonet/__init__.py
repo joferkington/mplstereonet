f2180dd9d1afed2d906c7e5fb73f93b70719e81e
Joe Kington <joferkington@gmail.com>
2012-05-15 10:28:26 -0500

    Updated __init__ with new functions in stereonet_math.

--- mplstereonet/__init__.py
+++ mplstereonet/__init__.py
@@ -1,9 +1,12 @@
 from stereonet_axes import StereonetAxes
 from stereonet_math import pole, plane, line, rake, plunge_bearing2pole
 from stereonet_math import geographic2pole, geographic2plunge_bearing
+from stereonet_math import xyz2stereonet, stereonet2xyz
+from stereonet_math import normal2pole, vector2plunge_bearing
 from contouring import contour_grid
 
 
 __all__ = ['StereonetAxes', 'pole', 'plane', 'line', 'rake', 
-           'plunge_bearing2pole', 'geographic2pole', 
-           'geographic2plunge_bearing', 'contour_grid']
+           'plunge_bearing2pole', 'geographic2pole', 'vector2plunge_bearing',
+           'geographic2plunge_bearing', 'contour_grid', 
+           'xyz2stereonet', 'stereonet2xyz', 'normal2pole']
