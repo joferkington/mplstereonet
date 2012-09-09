15849514b4319bacd353dd1536581f1c7663dc5b
Joe Kington <joferkington@gmail.com>
2012-09-09 17:33:03 -0500

    Incremented version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'mplstereonet',
-    version = '0.1',
+    version = '0.3-dev',
     description = "Stereonets for matplotlib",
     author = 'Joe Kington',
     author_email = 'joferkington@gmail.com',
