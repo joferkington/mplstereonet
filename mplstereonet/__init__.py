667d12e8421dca5d36dd3ce41657746292797947
Joe Kington <joferkington@gmail.com>
2013-03-30 09:47:38 -0500

    Made antipode functionality accessible through ``mplstereonet.antipode``

--- mplstereonet/__init__.py
+++ mplstereonet/__init__.py
@@ -4,6 +4,7 @@ from stereonet_math import geographic2pole, geographic2plunge_bearing
 from stereonet_math import xyz2stereonet, stereonet2xyz, azimuth2rake
 from stereonet_math import vector2plunge_bearing, vector2pole
 from stereonet_math import plane_intersection, project_onto_plane
+from stereonet_math import antipode
 from contouring import density_grid
 from utilities import parse_quadrant_measurement, parse_strike_dip, parse_rake
 from utilities import parse_azimuth, parse_plunge_bearing
@@ -13,7 +14,7 @@ from convenience_functions import subplots
 __all__ = ['StereonetAxes', 'pole', 'plane', 'line', 'rake', 
            'plunge_bearing2pole', 'geographic2pole', 'vector2plunge_bearing',
            'geographic2plunge_bearing', 'density_grid', 'plane_intersection',
-           'xyz2stereonet', 'stereonet2xyz', 'vector2pole', 
+           'xyz2stereonet', 'stereonet2xyz', 'vector2pole', 'antipode',
            'project_onto_plane', 'azimuth2rake', 'parse_azimuth',
            'parse_quadrant_measurement', 'parse_strike_dip', 'parse_rake',
            'parse_plunge_bearing', 'subplots']
