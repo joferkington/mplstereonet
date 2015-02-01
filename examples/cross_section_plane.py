803b32c6aa28431a57f1207ae5d5931913faa32f
Joe Kington <joferkington@gmail.com>
2015-02-01 13:34:01 -0600

    Stylistic changes to cross_section_plane example and added baseline image for example to tests

--- examples/cross_section_plane.py
+++ examples/cross_section_plane.py
@@ -7,20 +7,15 @@ The resulting plane is the optimal cross-section plane for this structure.
 The pole of the resulting plane would correspond to the intersection-linear
 when looking at schistosities or the fold-axis when looking at fold-hinges.
 """
-
 import matplotlib.pyplot as plt
 import numpy as np
 import mplstereonet
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='stereonet')
+fig, ax = mplstereonet.subplots()
 
 dip_directions = [100, 200]
 dips = [30, 40]
-
-strikes = []
-for x in dip_directions:
-    strikes.append(x-90)
+strikes = np.array(dip_directions) - 90
 
 ax.pole(strikes, dips, "bo")
 ax.plane(strikes, dips, color='black', lw=1)
@@ -30,6 +25,4 @@ fit_strike, fit_dip = mplstereonet.fit_girdle(strikes, dips)
 ax.plane(fit_strike, fit_dip, color='red', lw=1)
 ax.pole(fit_strike, fit_dip, marker='o', color='red', markersize=5)
 
-fig.tight_layout()
-#ax.grid()
 plt.show()
