dc97b59ac46fcf03ac345dee843e1c25c135fd28
Joe Kington <joferkington@gmail.com>
2016-04-10 20:05:07 -0500

    Update polar grid example

--- examples/polar_overlay.py
+++ examples/polar_overlay.py
@@ -1,15 +1,38 @@
+"""
+Demonstrates adding both polar and arbitrary grid overlays on a stereonet.
+Changing the grid overlay does not change the representation of the data.
+Notice that the plane, pole, and rake are all displayed identically in each
+case.  Only the grid lines change.
+"""
 import matplotlib.pyplot as plt
 import mplstereonet
 
-fig, ax = mplstereonet.subplots()
+def main():
+    # Display the data with a polar grid
+    ax1 = basic()
+    ax1.grid(kind='polar')
+    ax1.set_title('Polar overlay on a Stereonet', y=1.1)
 
-strike, dip = 315, 30
-ax.plane(strike, dip, color='lightblue')
-ax.pole(strike, dip, color='green', markersize=15)
-ax.rake(strike, dip, 40, marker='*', markersize=20, color='green')
+    # Display the data with a grid centered on the pole to the plotted plane.
+    ax2 = basic()
+    ax2.grid(center=mplstereonet.pole(315, 30))
+    ax2.set_title('Arbitrary overlay on a Stereonet', y=1.1)
 
-ax.grid(kind='polar')
-ax.set_title('Polar overlay on a Stereonet', y=1.1)
-fig.subplots_adjust(top=0.8)
+    plt.show()
 
-plt.show()
+def basic():
+    """Set up a basic stereonet and plot the same data each time."""
+    fig, ax = mplstereonet.subplots()
+
+    strike, dip = 315, 30
+    ax.plane(strike, dip, color='lightblue')
+    ax.pole(strike, dip, color='green', markersize=15)
+    ax.rake(strike, dip, 40, marker='*', markersize=20, color='green')
+
+    # Make a bit of room for the title...
+    fig.subplots_adjust(top=0.8)
+
+    return ax
+
+if __name__ == '__main__':
+    main()
