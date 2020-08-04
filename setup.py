d5b380d2f2f32df4ae628490e266174210fda7bb
Joe Kington <joferkington@gmail.com>
2020-08-04 10:54:39 -0500

    Be sure to include full description for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+import os
 from setuptools import setup
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(dirname, 'README.rst'), 'r') as infile:
+    long_description = infile.read()
 
 setup(
     name = 'mplstereonet',
     version = '0.7-dev',
     description = "Stereonets for matplotlib",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     author = 'Joe Kington',
     author_email = 'joferkington@gmail.com',
     license = 'MIT',
